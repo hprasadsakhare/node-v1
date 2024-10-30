917d9e13fede941fec9635b7dc376a0ac77abea1
Hariprasad Sakhare <hprasad.sakhareicloud.com@Hariprasads-MacBook-Pro.local>
2024-10-31 00:15:06 +0530

    new code

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,22 @@
+// thsi is code using routing for http
+
 const http = require('http');
 
 const server = http.createServer((req,res) =>{
-    console.log(req.url)
-    res.end('hello world')
+
+    if (req.url =="/about"){
+        res.end('the about page')
+    }
+
+    if (req.url =="/profile"){
+        res.end('the profile page')
+    }
+
+    if (req.url =="/"){
+        res.end('the home page')
+    }
+   // console.log(req.url)
+   // res.end('hello world')
 })
 
 server.listen(3001)
