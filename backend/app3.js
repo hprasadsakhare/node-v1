41a5f2393200ccc56860bb53839605e6d3fbb952
Hariprasad Sakhare <hprasad.sakhareicloud.com@Hariprasads-MacBook-Pro.local>
2024-11-01 14:39:36 +0530

    Resolved merge conflicts

--- backend/app3.js
+++ backend/app3.js
@@ -1,22 +1,41 @@
-const express = require ('express');
-const morgan = require ('morgan');
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+app.set('view engine', 'ejs');
 
-app.get('/about',(req,res)=>{
-    res.end('about page')
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-app.get('/profile',(req,res)=>{
-    res.end('profile page')
-})
 
-app.listen(3002)
+app.get('/about', (req, res) => {
+    res.end('about page');
+});
+
+app.get('/profile', (req, res) => {
+    res.end('profile page');
+});
+
+
+//app.get('/get-form-data', (req, res) => {
+//    console.log(req.query); 
+//    res.send('Data received');
+//  });
+
+app.post('/get-form-data', (req, res) => {
+    console.log(req.body); 
+    res.send('Data received');
+});
+
+
+
+app.listen(3002, () => {
+    console.log('Server is running on http://localhost:3002');
+});
