f67a7b4a4d46fc390d71507a0628caf082b629fc
Hariprasad Sakhare <hprasad.sakhareicloud.com@Hariprasads-MacBook-Pro.local>
2024-11-07 11:55:51 +0530

    Resolved merge conflicts

--- Drive/app.js
+++ Drive/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('Hello World');
-})
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render("index");
+});
 
-app.listen(3000,() =>{
-    console.log('server is runing on port 3000');
-})
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
