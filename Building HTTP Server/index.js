9fc336dc6ed7270f0fa7b05dcfac706d0d7670f5
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-11-08 23:57:14 +0530

    Resolved merge conflicts

--- Building HTTP Server/index.js
+++ Building HTTP Server/index.js
@@ -1,9 +1,8 @@
 const http = require("http");
 
-const myServer = http.createServer((req,res)=>{
-    console.log("new Req Rec.")
-    rec.end("Hello From Seerveer");
+const myServer = http.createServer((req, res) => {
+    console.log("New request received");
+    res.end("Hello from Server");
 });
 
-
-myServer.listen(8000,() => console.log("Server Started"));
+myServer.listen(8000, () => console.log("Server started on port 8000"));
