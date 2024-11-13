8820cd6a99f7b28460b1e1802ec13e1551c29992
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-11-14 01:47:43 +0530

    Resolved merge conflicts

--- Getting Started with Express/index.js
+++ Getting Started with Express/index.js
@@ -10,4 +10,8 @@ app.get('/about', (req, res) => {
     res.send("Hello From About Page");
 });
 
+app.get('/profile', (req, res)=>{
+    res.send("Profile Page");
+})
+
 app.listen(8000, () => console.log("Server started on port 8000"));
