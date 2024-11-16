0c33248049e3077526c1958841f12377259106d4
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-11-16 20:12:04 +0530

    add new contents

--- Project01 Rest API/index.js
+++ Project01 Rest API/index.js
@@ -19,16 +19,20 @@ app.get('/users', (req, res) => {
 });
 
 // Fetch user by ID
-app.get("/api/users/:id", (req, res) => {
-   const id = Number(req.params.id);
-   const user = users.find((user) => user.id === id);
+app.route('/api/users/:id').get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+ })
+ .put((req, res) => {
+    //edit user with id
+    res.json({status: "pending"})
+ })
+ .delete((req, res) => {
+    //delete user
+    res.json({status: "pending"})
+ });
 
-   // Handle user not found
-   if (!user) {
-       return res.status(404).json({ error: 'User not found' });
-   }
 
-   return res.json(user);
-});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
