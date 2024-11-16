0cf1ea50e7a5939aee747e2ce2f4dacda856c2f8
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-11-16 19:11:09 +0530

    Resolved merge conflicts

--- Project01 Rest API/index.js
+++ Project01 Rest API/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const users = require('./MOCK_DATA.json');
 const app = express();
 
 const PORT = 8000;
+
+app.get('/api/users', (req, res)=>{
+    return res.json(users);
+});
+
+app.get('/users', (req, res)=>{
+    return res.json(users);
+})
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
