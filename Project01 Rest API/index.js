1020e37d5a42a857ba2fc31715377cb68fd84d95
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-11-16 19:26:44 +0530

    add new contents

--- Project01 Rest API/index.js
+++ Project01 Rest API/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const PORT = 8000;
 
+// REST API
 app.get('/api/users', (req, res) => {
     return res.json(users);
 });
@@ -15,6 +16,19 @@ app.get('/users', (req, res) => {
     </ul>
     `;
     res.send(html);
+});
+
+// Fetch user by ID
+app.get("/api/users/:id", (req, res) => {
+   const id = Number(req.params.id);
+   const user = users.find((user) => user.id === id);
+
+   // Handle user not found
+   if (!user) {
+       return res.status(404).json({ error: 'User not found' });
+   }
+
+   return res.json(user);
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
