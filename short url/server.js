328c906a5463f0e34099a1c97265e5c0d6052446
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-11-20 22:55:24 +0530

    new

--- short url/server.js
+++ short url/server.js
@@ -24,6 +24,7 @@ app.get('/:shortId', async (req, res) => {
         if (!entry) {
             return res.status(404).json({ error: "Short URL not found" });
         }
+        
 
         res.redirect(entry.redirectUrl);
     } catch (error) {
