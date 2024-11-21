0a7ffc014d31a2e809f0eab70a234c9978f194a9
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-11-21 23:19:30 +0530

    new

--- Discord Bot/index.js
+++ Discord Bot/index.js
@@ -1,3 +1,9 @@
 const {Client, GatewayIntentBits} = require('discord.js');
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
+
+client.on('messageCreate', message =>{
+    console.log(message.content);
+})
+
+client.login('your-token-here');
