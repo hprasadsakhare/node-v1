abf46adba56520a9c4037c6298f17882037c5ee3
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-11-23 00:17:16 +0530

    new

--- Uploading Files/index.js
+++ Uploading Files/index.js
@@ -3,3 +3,14 @@ const express = require('express');
 
 const app = express();
  const PORT = 8000;
+
+ app.set('view engine', 'ejs');
+ app.set("views", path.resolve("views"));
+
+ app.use(express.json);
+
+ app.get('/', (req, res) => {
+    return res.render('homepage');
+ });
+
+ app.listen(PORT, () => console.log(`server started at PORT:8000`) );
