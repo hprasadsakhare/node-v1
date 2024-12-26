a269878bf6bde47dd80485cfe185c89c4fdce3fa
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2024-12-26 23:23:33 +0530

    new

---
+++ NodeJS Blogging Application/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+cosnt po = 8000;
+
+
+app.set('view engine', 'ejs');
+app.set("views",path.resolve("./views"));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
