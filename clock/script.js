5d01cf47446c8d0b31197592758d58de28082651
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-02-18 22:30:15 +0530

    new web

--- clock/script.js
+++ clock/script.js
@@ -7,8 +7,6 @@ function showTime(){
     document.getElementById("time").innerText = time;
 }
 
-// additional function 
-
 let interval = setInterval(showTime,1000);
 
 button.addEventListener('click',() => {
