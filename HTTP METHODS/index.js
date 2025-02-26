9bb1c2819818632d475b4074c6530c251a482737
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-02-26 12:50:40 +0530

    new cdoe

--- HTTP METHODS/index.js
+++ HTTP METHODS/index.js
@@ -2,9 +2,11 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+
 const myServer = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") return res.end();
 
+    
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
     const myUrl = url.parse(req.url, true);
 
