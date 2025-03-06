0ee318ec20a7635701742d323e2ce5c71dde0864
Hariprasad Sakhare <hprasad.sakhare@gmail.com>
2025-03-06 12:23:52 +0530

    adding

--- web3js/06_web3-eth/fixture/Greeter.ts
+++ web3js/06_web3-eth/fixture/Greeter.ts
@@ -10,6 +10,7 @@ web3.js is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
+
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
